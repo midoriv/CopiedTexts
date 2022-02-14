f66934bfd7d5ee43fee696cd55b33763ae3616dd
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-02-14 14:45:38 +1100

    Initial Commit

---
+++ CopiedTexts/CopiedTextsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopiedTextsApp.swift
+//  CopiedTexts
+//
+//  Created by Midori Verdouw on 14/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct CopiedTextsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
